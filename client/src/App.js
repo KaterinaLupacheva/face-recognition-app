d76702f9d9aad70fd4a84c9a817debe8f7950de9
Katerina <katerinalupacheva@gmail.com>
2020-02-11 17:45:23 +0300

    Cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -96,7 +96,6 @@ class App extends Component {
             .then(count => {
               this.setState(Object.assign(this.state.user, { entries: count}))
             })
-
         }
         this.displayFaceBox(this.calculateFaceLocation(response))
       })
