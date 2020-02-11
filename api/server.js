afe15763390def36968809e1581c75f2a982a0d9
Katerina <katerinalupacheva@gmail.com>
2020-02-11 16:55:38 +0300

    Bcrypt added

--- api/server.js
+++ api/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
 
 const app  = express();
 
@@ -11,7 +12,6 @@ const database = {
             id: '123',
             name: 'John',
             email: 'john@gmail.com',
-            password: 'cookies',
             entries: 0,
             joined: new Date()
         },
@@ -19,9 +19,15 @@ const database = {
             id: '124',
             name: 'Sally',
             email: 'sally@gmail.com',
-            password: 'bananas',
             entries: 0,
             joined: new Date()
+        }
+    ],
+    login: [
+        {
+            id: '987',
+            has: '',
+            email: 'john@gmail.com'
         }
     ]
 }
@@ -31,6 +37,9 @@ app.get('/', (req, res) => {
 });
 
 app.post('/signin', (req, res) => {
+    bcrypt.compare("cookies", '$2a$10$jr3sMndpOpPpgGqS5KC8T.HOSmKSbmMAZrToKxBR6BN/musFL6Os6', function(err, res) {
+        console.log('first guess', res)
+    });
     if(req.body.email === database.users[0].email &&
         req.body.password === database.users[0].password) {
             res.json('success');
@@ -42,6 +51,9 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
     const { email, name, password } = req.body;
+    bcrypt.hash(password, null, null, function(err, hash) {
+        console.log(hash);
+    });
     database.users.push({
         id: '125',
         name: name,
