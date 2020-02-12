a8b35ca7e98d9296f3dff68a409701b7fbfc279c
Katerina <katerinalupacheva@gmail.com>
2020-02-12 10:24:24 +0300

    Signin endpoint connected to db

--- api/server.js
+++ api/server.js
@@ -23,20 +23,27 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(cors());
 
-const database = {
-}
-
 app.get('/', (req, res) => {
-    res.send(database.users);
+    res.send('Hello');
 });
 
 app.post('/signin', (req, res) => {    
-    if(req.body.email === database.users[0].email &&
-        req.body.password === database.users[0].password) {
-            res.json(database.users[0]);
-    } else {
-        res.status(400).json('error login in')
-    }
+    db.select('email', 'hash').from('login')
+    .where('email', '=', req.body.email)
+        .then(data => {
+            const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+            if(isValid) {
+                return db.select('*').from('users')
+                    .where('email', '=', req.body.email)
+                    .then(user => {
+                        res.json(user[0])
+                    })
+                    .catch(err => res.status(400).json('Unable to get user'))
+            } else {
+                res.status(400).json('Wrong credentials');
+            }
+        })
+        .catch(err => res.status(400).json('Wrong credentials'));
 });
 
 app.post('/register', (req, res) => {
