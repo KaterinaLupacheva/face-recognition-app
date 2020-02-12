2549c928aaf8545114fa8fcbc88eaa2c14c839c2
Katerina <katerinalupacheva@gmail.com>
2020-02-12 09:25:34 +0300

    Knexjs and postgres added

--- api/server.js
+++ api/server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex');
+
+const postgres = knex({
+    client: 'pg',
+    connection: {
+        host : '127.0.0.1',
+        user : 'postgres',
+        password : '',
+        database : 'smart_brain'
+        }
+});
+
+console.log(postgres.select('*').from('users'));
 
 const app  = express();
 
